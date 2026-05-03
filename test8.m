34d8824577f2238886c6fcf6fa9e3f9023c691e7
Clem21sc <clement.schlegel21@gmail.com>
2026-05-03 19:17:08 +0200

    Update test8.m

--- test8.m
+++ test8.m
@@ -1,3 +1,5 @@
+
+clear; close all; clc;
 %% --- PARAMÈTRES PHYSIQUES ET GÉOMÉTRIQUES ---
 R = 0.01;       % Rayon du disque (1 cm)
 D = 0.01;       % Distance entre les disques (1 cm)
@@ -332,29 +334,8 @@ fprintf('\n');
 
 % Q11
 fprintf('Q11\n');
-fprintf('Regarder figure 6\n');
-
-
-
-for D = D_values_tot
-    epsilon = D/R_val;
-    K_new = @(x,y)(1/pi) * (1./(epsilon^2 + (x-y).^2) + 1./(epsilon^2 + (x+y).^2));
-    
-    n_pts = 100;
-    [x_u, u_sol] = solve_linear_gauss(n_pts, K_new, 0, 1);
-    
-    % Calculs des capacités
-    C_Love = 4 * epsilon0 * R * trapz(x_u, u_sol);
-    C_class = (epsilon0 * S) / D;
-
-    figure(6); hold on;
-    plot(x_u, u_sol, 'DisplayName', ['D = ' num2str(D*1000) ' mm']);
-end
-
-
-title('Influence de la distance sur la densité de charge u(x) pour la resolution gaussienne');
-xlabel('x (normalisé)'); ylabel('u(x)');
-legend; grid on; 
+fprintf('Regarder figure 7 , 9 & 10\n');
+ 
 
 for D = D_values_eff
     epsilon = D/R_val;
@@ -376,26 +357,6 @@ title('Influence de la distance sur la densité de charge u(x) pour la resolutio
 xlabel('x (normalisé)'); ylabel('u(x)');
 legend; grid on; 
 
-for D = D_values_tot
-    epsilon = D/R_val;
-    K_new = @(x,y)(1/pi) * (1./(epsilon^2 + (x-y).^2) + 1./(epsilon^2 + (x+y).^2));
-    
-    n_pts = 100;
-    [x_u, u_sol] = solve_linear_classic(n_pts, K_new, 0, 1);
-    
-    % Calculs des capacités
-    C_Love = 4 * epsilon0 * R * trapz(x_u, u_sol);
-    C_class = (epsilon0 * S) / D;
-    
-
-    % Tracer u(x) pour comparer les profils
-    figure(8); hold on;
-    plot(x_u, u_sol, 'DisplayName', ['D = ' num2str(D*1000) ' mm']);
-end
-
-title('Influence de la distance sur la densité de charge u(x) pour la resolution lineaire');
-xlabel('x (normalisé)'); ylabel('u(x)');
-legend; grid on; 
 
 
 for D = D_values_eff
@@ -419,4 +380,230 @@ title('Influence de la distance sur la densité de charge u(x) pour la resolutio
 xlabel('x (normalisé)'); ylabel('u(x)');
 legend; grid on; 
 
+for D = D_values_eff
+    epsilon = D/R_val;
+    K_new = @(x,y)(1/pi) * (1./(epsilon^2 + (x-y).^2) + 1./(epsilon^2 + (x+y).^2));
+    
+    n3 = 30; % Nombre de points
+    x3 = linspace(a, b, n3); % Points
+    A3 = build_A_matrix(x3,K_new,a,b,n3);
+    I3 = eye(n3);
+    d3 = ones(n3, 1); % Terme source d(x) = 1
+
+    u_new = (I3 - A3) \ d3;
+    
+    % Calculs des capacités
+    C_Love = 4 * epsilon0 * R * trapz(x3, u_new);
+    C_class = (epsilon0 * S) / D;
+    
+
+    % Tracer u(x) pour comparer les profils
+    figure(10); hold on;
+    plot(x3, u_new, 'DisplayName', ['D = ' num2str(D*1000) ' mm']);
+end
+
+title('Influence de la distance sur la densité de charge u(x) pour la resolution avec polynôme de Lagrange globaux');
+xlabel('x (normalisé)'); ylabel('u(x)');
+legend; grid on; 
+
 fprintf('\n');
+
+%% ============================
+%  MÉTHODE D'ADOMIAN 
+% =============================
+
+%% --- Paramètres numériques ---
+N_ad = 100;             
+x_ad = linspace(a,b,N_ad);  
+quad_order = 40;      % Quadrature Gauss-Legendre
+[uq, wq] = gauss_legendre(quad_order);
+
+% Transformation vers [0,1]
+yq = 0.5*(b-a)*uq + 0.5*(b+a);
+wq = 0.5*(b-a)*wq;
+
+%% --- Décomposition d'Adomian ---
+max_iter = 40;        % Nombre de termes de la série
+U_terms = zeros(max_iter, N_ad);
+
+% Terme initial : u0(x) = 1
+U_terms(1,:) = ones(1,N_ad);
+
+for n = 1:max_iter-1
+    u_prev = U_terms(n,:);
+    u_next = zeros(1,N_ad);
+
+    for i = 1:N_ad
+        xi = x_ad(i);
+        u_next(i) = sum( K(xi, yq) .* interp1(x_ad, u_prev, yq, 'linear') .* wq );
+    end
+
+    U_terms(n+1,:) = u_next;
+end
+
+u_adomian = sum(U_terms,1);
+
+figure(11);
+plot(x_ad, u_adomian, 'LineWidth', 2);
+xlabel('x'); ylabel('u(x)');
+title('Méthode d''Adomian appliquée à l''équation de Love');
+grid on;
+
+errors3 = zeros(max_iter,1);
+
+for k = 1:max_iter
+    u_adomian2 = sum(U_terms(1:k,:),1);
+    u_ref_interp = interp1(x_ref, u_ref, x_ad);
+    errors3(k) = max(abs(u_adomian2 - u_ref_interp));
+end
+
+figure(12);
+semilogy(1:max_iter, errors3, 'b-o', 'LineWidth', 2);
+xlabel('Nombre de termes N');
+ylabel('Erreur ||u^{(N)} - u_{ref}||_\infty');
+title('Convergence lente de la méthode d''Adomian');
+grid on;
+
+%% ============================
+%  MÉTHODE COLLOCATION
+% =============================
+
+% =========================================================================
+% FONCTION : cheb4_poly
+% ROLE : Calcule tous les polynômes de Tchebychev
+%        W_0(x), W_1(x), ..., W_N(x) en un point donné x.
+% -------------------------------------------------------------------------
+% DESCRIPTION :
+%   Cette fonction renvoie un vecteur ligne contenant les valeurs des
+%   polynômes W_i(x) pour i = 0..N.
+%
+%
+% ENTREE :
+%   x : (scalaire) Point où évaluer les polynômes.
+%   N : (entier) Degré maximal des polynômes à calculer.
+%
+% SORTIE :
+%   W : (vecteur 1×(N+1)) contenant [W_0(x), W_1(x), ..., W_N(x)].
+%
+% =========================================================================
+
+function W = cheb4_poly(x,N)
+    W = zeros(1,N+1);
+    W(1) = 1;         
+    if N >= 1
+        W(2) = 2*x + 1; 
+    end
+    for n = 2:N
+        W(n+1) = 2*x*W(n) - W(n-1);
+    end
+end
+
+% =========================================================================
+% FONCTION : cheb4_poly_single
+% ROLE : Calcule le polynôme de Tchebychev W_i(x)
+%        pour un degré donné i et pour une valeur x.
+% -------------------------------------------------------------------------
+% DESCRIPTION :
+%   Cette fonction implémente la définition récursive des polynômes de
+%   Tchebychev
+%
+% ENTREE :
+%   x : (scalaire ou vecteur) Point(s) où évaluer le polynôme.
+%   i : (entier) Degré du polynôme W_i.
+%
+% SORTIE :
+%   Wi : (scalaire ou vecteur) Valeur(s) du polynôme W_i(x).
+%
+% =========================================================================
+
+function Wi = cheb4_poly_single(x,i)
+    x = x(:)';
+    if i == 0
+        Wi = ones(size(x));
+        return;
+    elseif i == 1
+        Wi = 2*x + 1;
+        return;
+    end
+
+    % Initialisation
+    Wm2 = ones(size(x));  
+    Wm1 = 2*x + 1;            
+
+    % Récurrence vectorielle
+    for n = 2:i
+        Wn = 2.*x.*Wm1 - Wm2;
+        Wm2 = Wm1;
+        Wm1 = Wn;
+    end
+
+    Wi = Wm1;
+end
+
+%%----Résolution de l'équation de Love ------
+function [x_cheb , u_cheb]=solve_love_cheb4(N_cheb,K,a,b)                    
+    x_cheb = linspace(a,b,N_cheb+1);    
+    
+    V = zeros(N_cheb+1,N_cheb+1);
+    I = ones(N_cheb+1,1);  
+
+    for j = 1:N_cheb+1
+        xj = x_cheb(j);
+        W_at_xj = cheb4_poly(xj,N_cheb);  
+        
+        for i = 0:N_cheb
+            % W_i(xj)
+            Wi_xj = W_at_xj(i+1);
+            
+            integrand_i = @(t) K(xj,t) .* cheb4_poly_single(t,i);
+            
+            I_i = integral(integrand_i,0,1,'AbsTol',1e-10,'RelTol',1e-8);
+    
+            V(j,i+1) = Wi_xj - I_i;
+        end
+    end
+    C = V \ I;
+    u_cheb = zeros(size(x_cheb));
+    
+    for k = 1:length(x_cheb)
+        W_vals = cheb4_poly(x_cheb(k),N_cheb);   
+        u_cheb(k) = W_vals * C;           
+    end
+end
+
+[x_cheb,u_cheb]=solve_love_cheb4(100,K,a,b);
+
+figure(13);
+plot(x_cheb,u_cheb,'b-','LineWidth',2);
+xlabel('x');
+ylabel('u(x)');
+title('Méthode de collocation de Tchebychev ');
+grid on;
+
+N_ref = 300;   
+[x_ref, u_ref] = solve_love_cheb4(N_ref,K,a,b);
+
+N_values = 2:2:40;    
+errors = zeros(size(N_values));
+
+%% Calcul de l'erreur pour chaque N
+for k = 1:length(N_values)
+    N = N_values(k);
+
+    [xN, uN] = solve_love_cheb4(N,K,a,b);
+
+    u_ref_interp = interp1(x_ref, u_ref, xN, 'linear');
+
+    errors(k) = max(abs(uN - u_ref_interp));
+end
+
+figure(14);
+semilogy(N_values, errors, 'b-', 'LineWidth', 2, 'MarkerSize', 8);
+xlabel('Degré N des polynômes de Tchebychev');
+ylabel('Erreur ||u_N - u_{ref}||_\infty');
+title('Convergence de la méthode de collocation');
+grid on;
+
+
+
+
